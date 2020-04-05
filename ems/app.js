8d36f709b72ed8a6d02a82a4e9dba2f798fec131
Christine Bohnet <christine.bohnet@gmail.com>
2020-04-05 13:40:10 -0500

    week 7 assignments

--- ems/app.js
+++ ems/app.js
@@ -1,79 +1,92 @@
-/*
-============================================
-; Title:  app.js
-; Author: Professor Krasso
-; Date:   19 March 2020
-; Modified by: Chris Bohnet
-; Description: Layouts are an important part of building flexible and highly scalable User Interfaces. Over the next five weeks 
-; we are going to be building a simple Node.js application for employee records. Milestone 1 is to build the websites layouts and navigation.
-; 
-; There are many ways to build layouts in Express with EJS. But, the main components are:
-; 
-; Header
-; Navigation
-; Footer
-; Scripts
-; 
-; The header.ejs file will contain the head element and stylesheet references. Navbar.ejs will contain the website's navigation, 
-; footer.ejs will contain the applications main footer section, and scripts.ejs will contain the websites JavaScript references.
-; 
-; Structuring your application this way provides several advantages:
-; 
-; Stylesheet references are updatable from a centralized location
-; Navigation is updatable from a centralized location
-; JavaScript references are updatable from a centralized location
-; Decoupling of HTML content
-;  
-;===========================================
-*/
-
-var header = require('../bohnet-header.js');
-console.log(header.display('Christine', 'Bohnet', 'app.js'))
-
-//library that allows us to use express
 var express = require("express");
-//library that allows us to create the server
+
 var http = require("http");
-//library that allows us to use morgan logging
-var logger = require("morgan");
 
 var path = require("path");
 
-//Calls the express function to start a new Express application
-app = express();
+var mongoose = require("mongoose");
 
-//Tell Express the views are in the views directory
+var logger = require("morgan");
+
+var Employee = require("./models/employee");
+
+
+// mLab connection
+var mongoDB = "mongodb+srv://web340MongoUser:web340MongoUser@cluster0-3uuui.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoDB, {
+
+    useMongoClient: true
+
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+db.once("open", function() {
+
+    console.log("Application connected to mLab MongoDB instance");
+
+});
+
+var app = express();
+
 app.set("views", path.resolve(__dirname, "views"));
+
 app.set("view engine", "ejs");
 
 app.use(logger("short"));
+// model
+var employee = new Employee({
 
-//Create an array of employees
-var emp = ["Chris", "Brian", "Georgie", "Sam"];
+    firstName: "Chris",
+    lastName: "Bohnet"
+
+});
 
 
-app.get("/", function(request, response) {
-  response.render("index", {
-    title: "Home page",
-    employee: [
-      {name: "Chris"}, 
-      {name: "Brian"}, 
-      {name: "Georgie"},
-      {name:  "Sam"}]
-    //employee: {name: Chris}, {name:Sam}
-    /*myObj = {
-      "name":"John",
-      "age":30,
-      "cars": [
-        {"name":"Ford", "models":["Fiesta", "Focus", "Mustang"]},
-        {"name":"BMW", "models":["320", "X3", "X5"]},
-        {"name":"Fiat", "models":["500", "Panda"] }
-      ]
-    }
-    */
-  });
+/*var employee = [
+    {name: "Chris"}, 
+    {name: "Brian"}, 
+    {name: "Georgie"},
+    {name:  "Sam"}];
+*/
+app.get("/", function (request, response) {
+
+    response.render("index", {
+
+        title: "Home page",
+        employee: employee
+    });
+
+});
+
+app.get("/new", function (request, response) {
+
+    response.render("new", {
+
+        title: "Enter New Employee",
+        
+    });
+
+});
+
+app.get("/list", function (request, response) {
+
+    response.render("list", {
+
+        title: "List of Employees",
+        employee: employee
+    });
+
 });
 
 http.createServer(app).listen(8080, function() {
-  console.log("Application started on port 8080!");
+
+    console.log("Application started on port 8080!");
+
 });
+
---
+++ ems/models/employee.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title:  employee.js
+; Author: Chris Bohnet
+; Date:   4 April 2020
+; Description: File for the Employee database model
+;===========================================
+*/
+
+// Require statements
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Employee Schema
+let EmployeeSchema = new Schema({
+  firstName: {type: String, required: true },
+  lastName: {type: String, required: true }
+});
+
+
+// define the employee model
+// Export the model so its publicly available.
+module.exports = mongoose.model('Employee', EmployeeSchema);
